739d25106f4bc5a897da1f981471fb1c8390e2eb
Vincent Song <dela.vincent.15@gmail.com>
2025-12-10 08:02:11 -0500

    Finish extraction of point clouds with updated bounding boxes

--- src/output_file_visualization/visualize_ply_file.py
+++ src/output_file_visualization/visualize_ply_file.py
@@ -2,7 +2,7 @@ import os
 import open3d as o3d
 
 FILE_PATH = None
-FOLDER_PATH = "waymo_vehicle_ground_truth_occluded/all_vehicle_point_clouds_50_min_points_occluded_gt/incomplete"
+FOLDER_PATH = "waymo_vehicle_incomplete_pointclouds/all_vehicle_point_clouds_50_min_points_updated_bb"
 
 def visualize_single(file_path):
     pc = o3d.io.read_point_cloud(file_path)
